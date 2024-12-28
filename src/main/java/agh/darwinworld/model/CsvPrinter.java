7fdbbd5555ddfab1075994f2588cf602bf116a73
Jakub Jurczyk <jjurczyk@student.agh.edu.pl>
2024-12-28 19:59:21 +0100

    fix: minor fixes

--- src/main/java/agh/darwinworld/model/CsvPrinter.java
+++ src/main/java/agh/darwinworld/model/CsvPrinter.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class CsvPrinter implements SimulationStepListener, SimulationPauseListener {
-    private String content;
+    private String content = "";
     private final String filename;
 
     public CsvPrinter(String filename) {
