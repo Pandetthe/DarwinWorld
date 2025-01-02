46ffefbc6f792d9165811c46c92cceff93dc1604
Jakub Jurczyk <jjurczyk@student.agh.edu.pl>
2025-01-02 11:58:18 +0100

    docs: add javadoc to Simulation

--- src/main/java/agh/darwinworld/Simulation.java
+++ src/main/java/agh/darwinworld/Simulation.java
@@ -5,6 +5,10 @@ import agh.darwinworld.models.listeners.SimulationStepListener;
 import agh.darwinworld.models.maps.AbstractMap;
 import agh.darwinworld.presenters.SimulationPresenter;
 
+/**
+ * The {@code Simulation} class represents a runnable simulation of a Darwinian world.
+ * It manages the lifecycle of the simulation and interacts with the map and its elements.
+ */
 public class Simulation implements Runnable {
     private final SimulationParameters params;
     private final AbstractMap map;
@@ -12,6 +16,11 @@ public class Simulation implements Runnable {
     private int step = 1;
     private boolean isRunning = false;
 
+    /**
+     * Constructs a {@code Simulation} with the specified parameters.
+     *
+     * @param params the parameters for the simulation
+     */
     public Simulation(SimulationParameters params) {
         this.params = params;
         this.map = params.mapType().createMap();
@@ -20,6 +29,11 @@ public class Simulation implements Runnable {
         map.growPlants(params.startingPlantAmount());
     }
 
+    /**
+     * Returns the parameters of the simulation.
+     *
+     * @return the simulation parameters
+     */
     public SimulationParameters getParameters() {
         return this.params;
     }
@@ -44,26 +58,52 @@ public class Simulation implements Runnable {
         }
     }
 
+    /**
+     * Starts the simulation.
+     */
     public void start() {
         isRunning = true;
     }
 
+    /**
+     * Stops the simulation.
+     */
     public void stop() {
         isRunning = false;
     }
 
+    /**
+     * Checks if the simulation is running.
+     *
+     * @return {@code true} if the simulation is running, otherwise {@code false}.
+     */
     public boolean isRunning() {
         return isRunning;
     }
 
+    /**
+     * Adds a step listener to the simulation.
+     *
+     * @param listener the listener to be added
+     */
     public void addStepListener(SimulationStepListener listener) {
         map.addStepListener(listener);
     }
 
+    /**
+     * Removes a step listener from the simulation.
+     *
+     * @param simulationPresenter the listener to be removed
+     */
     public void removeStepListener(SimulationPresenter simulationPresenter) {
         map.removeStepListener(simulationPresenter);
     }
 
+    /**
+     * Returns the map used in the simulation.
+     *
+     * @return the simulation map
+     */
     public AbstractMap getMap() {
         return this.map;
     }
